dee8ab75e8977c76f11b7683073222a5325fc5b4
Somya <isomya13@gmail.com>
2023-02-24 02:17:58 +0530

    rendering templates now

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+SALARIES = [
+    "1L", "2L", "3L", "1L", "1.5"
+]
+
+
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return render_template('home.html', salaries= SALARIES)
 
 
 print(__name__)
